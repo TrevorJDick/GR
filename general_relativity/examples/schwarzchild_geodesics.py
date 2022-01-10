b5b890e4b6d1a9d5940c0e46f0f1bed8a1abd777
TrevorJDick <thefriendyfew@gmail.com>
2022-01-10 00:29:27 -0700

    updated schwarzchild geodesic example to show use caes of 2 test particles

--- general_relativity/examples/schwarzchild_geodesics.py
+++ general_relativity/examples/schwarzchild_geodesics.py
@@ -12,13 +12,19 @@ q, dq, line_element, params_sym = met.schwarzchild_metric()
 
 # intial conditions
 metric_tensor_params = [1] # M=1
-q0 = np.array([0, 40, np.pi / 2, 0])
+q0 = np.array(
+    [[0, 40, np.pi / 2, 0],
+    [0, 40, np.pi / 2, 0]]
+).T
 # initial 3-momentum
-p0 = np.array([0, 0, 3.83405])
+p0 = np.array(
+    [[0, 0, 3.83405],
+    [0, 0, 1.83405]]
+).T
 
 # geodesic
-n_timesteps = 5500 * 3
-delta = 0.5 ** 3
+n_timesteps = 5500 * 2
+delta = 0.5 ** 2
 geod = geodesic_from_metric(
     q, 
     dq,
@@ -30,7 +36,7 @@ geod = geodesic_from_metric(
     n_timesteps,
     delta, 
     omega=1,
-    order=2 * 3, 
+    order=2 * 2, 
     timelike=True,
     solve_p0_zeroth_term=True,
     neg_g_inv=True
@@ -41,7 +47,7 @@ geod = geodesic_from_metric(
 
 # two phase space should convege so will just pick first phase space
 ## can only choose 1 particle
-i = 0
+i = 1
 eqns_motion = np.array([t[0, :, i] for t in geod])
 del i
 # stock coords
