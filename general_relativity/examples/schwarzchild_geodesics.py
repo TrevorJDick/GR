5fc4efa751012eb1c0cb62211c4eea08a498a9f1
TrevorJDick <thefriendyfew@gmail.com>
2022-01-17 02:23:02 -0700

    better handling of inital 4 momentum, updated some examples to follow suit, still need to work on initial condition matching with solving for initial energy compoent of 4 momentum

--- general_relativity/examples/schwarzchild_geodesics.py
+++ general_relativity/examples/schwarzchild_geodesics.py
@@ -12,19 +12,40 @@ q, dq, line_element, params_sym = met.schwarzchild_metric()
 
 # intial conditions
 metric_tensor_params = [1] # M=1
-q0 = np.array(
+Q0 = np.array(
     [
      [0, 40, np.pi / 2, 0],
       [0, 40, np.pi / 2, 0]
      ]
 ).T
+Q0 = np.concatenate(
+    [
+        Q0,
+        np.concatenate(
+            [
+                np.zeros(10).reshape(1, -1),
+                np.random.normal(loc=0, scale=10, size=(3, 10))
+            ],
+            axis=0
+        )
+    ],
+    axis=1
+)
+
 # initial 3-momentum
-p0 = np.array(
+P0 = np.array(
     [
      [0, 0, 3.83405],
       [0, 0, 4.83405]
      ]
 ).T
+P0 = np.concatenate(
+    [
+        P0,
+        np.random.normal(loc=0, scale=2, size=(3, 10))
+    ],
+    axis=1
+)
 
 # geodesic
 n_timesteps = 5500 * 1
@@ -34,8 +55,8 @@ geod = geodesic_from_metric(
     dq,
     line_element,
     params_sym,
-    q0, 
-    p0, 
+    Q0, 
+    P0, 
     metric_tensor_params,
     n_timesteps,
     delta, 
@@ -50,20 +71,36 @@ geod = geodesic_from_metric(
 ### for plotting ###
 
 # two phase space should convege so will just pick first phase space
-## can only choose 1 particle
-i = 1
-eqns_motion = np.array([t[0, :, i] for t in geod])
-del i
-# stock coords
-# x = eqns_motion[:, 1]
-# y = eqns_motion[:, 2]
-# z = eqns_motion[:, 3]
+eqns_motion = np.array([g[0, :, :] for g in geod]) # shape (n_timesteps, n_coords, n_particles)
 
 # convert to cartesian for Schwarzchild/Kerr
+def schwarzchild_to_cartesian(eqns_motion, a):
+    cart_eqns_motion = np.zeros_like(eqns_motion)
+    cart_eqns_motion[:, 0, :] = eqns_motion[:, 0, :]
+    # Eddington–Finkelstein coordinates
+    cart_eqns_motion[:, 1, :] = (
+        np.sqrt(a ** 2 + eqns_motion[:, 1, :] ** 2) 
+        * np.sin(eqns_motion[:, 2, :]) 
+        * np.cos(eqns_motion[:, 3, :])
+    )
+    cart_eqns_motion[:, 2, :] = (
+        np.sqrt(a ** 2 + eqns_motion[:, 1, :] ** 2)
+        * np.sin(eqns_motion[:, 2, :]) 
+        * np.sin(eqns_motion[:, 3, :])
+    )
+    cart_eqns_motion[:, 3, :] = eqns_motion[:, 1, :]  * np.cos(eqns_motion[:, 2, :])
+    return cart_eqns_motion
+
 a = 0 # schwarzchild
-x = np.sqrt(a ** 2 + eqns_motion[:, 1] ** 2) * np.sin(eqns_motion[:, 2]) * np.cos(eqns_motion[:, 3])
-y = np.sqrt(a ** 2 + eqns_motion[:, 1] ** 2) * np.sin(eqns_motion[:, 2]) * np.sin(eqns_motion[:, 3])
-z = eqns_motion[:, 1]  * np.cos(eqns_motion[:, 2])
+cart_eqns_motion = schwarzchild_to_cartesian(eqns_motion, a)
+print(f'{cart_eqns_motion.shape} -- shape of equations of motion')
 
+# plot single particle
+i = 0
+bgp.geodesic_plotter_3d(
+    cart_eqns_motion[:, 1, i],
+    cart_eqns_motion[:, 2, i],
+    cart_eqns_motion[:, 3, i],
+    axes_names=['X', 'Y', 'Z']
+)
 
-bgp.geodesic_plotter_3d(x, y, z, axes_names=['X', 'Y', 'Z'])
--- general_relativity/utils/initial_momentum.py
+++ general_relativity/utils/initial_momentum.py
@@ -13,51 +13,66 @@ def solve_energy_term_initial_4_momentum(Q0, P0, g_sym_inv,
     Solve the equation, where P = (p0, p1, p1, p3),
     (P^T).(g^-1).P = (m0*c)^2 for p0 when p1, p2, p3 are given.
     
+    space-like not implimented only timelike and null like (timelike false)
     """
     res = []
     for q0, p0 in zip(Q0.T, P0.T):
         p0_sym = sym.zeros(4, 1)
         p0_sym[0, 0] = sym.symbols('p_0')
         p0_sym[1:, 0] = p0
-        try:
-            # solve for p_0 in initial 4-momentum p0
-            g_sym_inv0 = cnv.symbolic_obj_subs(
-                g_sym_inv,
-                [metric_tensor_params_sym, q], # symbolic variables
-                [metric_tensor_params, q0] # variables
-            )
-            
-            # add 1 for timelike
-            p_0_poly = (
-                np.dot(np.dot(p0_sym.T, g_sym_inv0), p0_sym).flatten()[0]
-                + int(timelike)
-            )
-            p_0 = sorted(sym.solve(p_0_poly, 'p_0'))[0]
-            p0_sym[0] = p_0
-            p0 = cnv.symbolic_const_matrix_to_numpy(p0_sym).flatten()
-            res.append(p0)
-        except TypeError as e:
+        
+        # solve for time component p_0, in initial 4-momentum p0
+        g_sym_inv0 = cnv.symbolic_obj_subs(
+            g_sym_inv,
+            [metric_tensor_params_sym, q], # symbolic variables
+            [metric_tensor_params, q0] # variables
+        )
+        # polynomial for P^2 = P_alpha g^{alpha beta} P_beta
+        p_0_poly = (
+            np.dot(np.dot(p0_sym.T, g_sym_inv0), p0_sym).flatten()[0]
+        )
+        # add 1 for timelike
+        p_0 = sym.solve(p_0_poly + int(timelike), 'p_0')
+        
+        if isinstance(p_0[0], sym.Mul):
             print(
-                f'CAUGHT: {e}\n'
+                'CAUGHT: p_0_poly solution is not float.\n'
                 'Trying g_sym_inv = -g_sym_inv...\n'
             )
             g_sym_inv = -g_sym_inv
-            # solve for p_0 in initial 4-momentum p0
             g_sym_inv0 = cnv.symbolic_obj_subs(
                 g_sym_inv,
                 [metric_tensor_params_sym, q], # symbolic variables
                 [metric_tensor_params, q0] # variables
             )
-            
-            # add 1 for timelike
+            # polynomial for P^2 = P_alpha g^{alpha beta} P_beta
             p_0_poly = (
                 np.dot(np.dot(p0_sym.T, g_sym_inv0), p0_sym).flatten()[0]
-                + int(timelike)
             )
-            p_0 = sorted(sym.solve(p_0_poly, 'p_0'))[0]
-            p0_sym[0] = p_0
+            # add 1 for timelike
+            p_0 = sym.solve(p_0_poly + int(timelike), 'p_0')
+            if isinstance(p_0[0], sym.Mul):
+                print(
+                    'CAUGHT: p_0_poly solution is not float.\n'
+                    'Already tried g_sym_inv = -g_sym_inv!\n'
+                    'The given initial 4-position and 3-momentum are '
+                    f'likely not consistent with a timelike={timelike} curve.\n'
+                    'Returning a nan p_0 energy component...\n'   
+                )
+                p0 = cnv.symbolic_const_matrix_to_numpy(
+                    p0_sym.subs('p_0', np.nan)
+                ).flatten()
+            else:
+               if p_0[0] < 0:
+                   p0_sym[0] = p_0[0]
+               else:
+                   p0_sym[0] = p_0[1]
+               p0 = cnv.symbolic_const_matrix_to_numpy(p0_sym).flatten() 
+        else:
+            if p_0[0] < 0:
+                p0_sym[0] = p_0[0]
+            else:
+                p0_sym[0] = p_0[1]
             p0 = cnv.symbolic_const_matrix_to_numpy(p0_sym).flatten()
-            res.append(p0)
+        res.append(p0)
     return np.vstack(res).T
-
-
